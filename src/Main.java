39526eb65b49c8202cd0959515960b0f30fe95c0
yasmine1110 <yasminemariamd@gmail.com>
2026-05-12 00:25:59 +0200

    modification des donnee pour le text dans main

--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,36 @@
 
         // ajouter Contact 1
         Contact myContact = new Contact();
-        myContact.name        = "Jean Baptiste";
+        myContact.name        = "abdoul karim";
         myContact.phoneNumber = "0758493021";
         myContactsManager.addContact(myContact);
 
         // ajouter Contact 2
         Contact contact2 = new Contact();
-        contact2.name        = "Marie Dupont";
+        contact2.name        = "Mariam kone";
         contact2.phoneNumber = "0612345678";
         myContactsManager.addContact(contact2);
 
         // ajouter Contact 3
         Contact contact3 = new Contact();
-        contact3.name        = "Kofi Mensah";
-        contact3.phoneNumber = "0723456789";
+        contact3.name        = "tossah fafa";
+        contact3.phoneNumber = "0717306789";
         myContactsManager.addContact(contact3);
 
         // Contact 4
         Contact contact4 = new Contact();
-        contact4.name        = "Aminata Bah";
+        contact4.name        = "Aminata ouattara";
         contact4.phoneNumber = "0698765432";
         myContactsManager.addContact(contact4);
 
         // ajouter Contact 5
         Contact contact5 = new Contact();
-        contact5.name        = "Lucas Martin";
+        contact5.name        = "koffiMartin";
         contact5.phoneNumber = "0745678901";
         myContactsManager.addContact(contact5);
 
         //  Rechercher un contact et afficher son numéro
-        Contact found = myContactsManager.searchContact("Kofi Mensah");
+        Contact found = myContactsManager.searchContact("tossah fafa");
 
         if (found != null) {
             System.out.println("Contact trouvé !");
